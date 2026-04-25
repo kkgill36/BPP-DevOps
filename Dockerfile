bf808333a95b2c6c4ba08c09f404b9b0003195b6
Kiran Gill <kiran.gill3620@gmail.com>
2026-04-25 20:11:07 +0100

    hardening for scanners

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,20 @@
-FROM python:3.11-slim
+FROM python:3.11-slim-bookworm
 
 WORKDIR /app
 
 COPY requirements.txt .
-RUN pip install --no-cache-dir -r requirements.txt
+
+RUN pip install --upgrade pip setuptools wheel && \
+    pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
+RUN useradd -m appuser && chown -R appuser:appuser /app
+
+USER appuser
+
 EXPOSE 5001
+
+HEALTHCHECK CMD curl --fail http://localhost:5001/health || exit 1
 
 CMD ["python3", "app.py"]
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,23 +1,28 @@
 variable "resource_group_name" {
     type    = string
     default = "rg-bpp-devops"
+    description = "name of resource group"
 }
 
 variable "location" {
     type    =string
     default = "uksouth"
+    description = "location set"
 }
 
 variable "cluster_name"{
     type = string
     default = "aks-bpp-devops"
+    description = "name of cluster"
 
 }
 
 variable "node_count"{
     default = 1
+    description = "number of nodes"
 }
 
 variable "vm_size"{
     default = "Standard_D2pls_v6"
+    description = "aks vm size"
 }
